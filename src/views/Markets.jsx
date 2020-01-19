ff751fe963acf7cbc64a40f67c0ff4c735f7839a
Carlos Alba <soradev4@gmail.com>
2020-01-19 09:26:53 -0800

    client market config ensenada

--- src/views/Markets.jsx
+++ src/views/Markets.jsx
@@ -1,31 +1,34 @@
-
-import React from "react";
+import React, { useState } from "react";
 import SideNav from "../components/SideNav";
-import {Col, Container, Row} from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import Loading from "../components/Loading";
+import ApolloClient, { gql } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const client = new ApolloClient({
+  uri: "https://ensenada-fxuevruz4a-uc.a.run.app/",
+  cache: new InMemoryCache()
+});
 
 class Markets extends React.Component {
+  render() {
+    return (
+      <>
+        <SideNav />
 
-    render() {
-
-        return (
-            <>
-                <SideNav/>
-
-                <Container className="pt-lg-md main">
-                    <Row className="justify-content-center">
-                        <Col lg="10" className="category">
-                            <h1>Markets</h1>
-                            <div>
-                                <Loading/>
-
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
-    }
+        <Container className="pt-lg-md main">
+          <Row className="justify-content-center">
+            <Col lg="10" className="category">
+              <h1>Markets</h1>
+              <div>
+                <Loading />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default Markets;
