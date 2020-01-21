e551068604714ec3e17a19d098ecf31a117da0e1
Carlos Alba <soradev4@gmail.com>
2020-01-21 13:04:32 -0800

    Lazy Loading Images

--- src/views/News.jsx
+++ src/views/News.jsx
@@ -1,74 +1,66 @@
-
 import React from "react";
 import SideNav from "../components/SideNav";
-import {Card, CardBody, Col, Container, Row } from "reactstrap";
+import { Card, CardBody, Col, Container, Row } from "reactstrap";
 import Loading from "../components/Loading";
 
 class News extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loadNews: false };
+    this.timeout = null;
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {loadNews: false};
-        this.timeout = null;
-    }
+  loadNews() {
+    this.setState({ loadNews: true });
+  }
 
-    loadNews() {
+  render() {
+    this.timeout = setTimeout(() => this.loadNews(), 1000);
 
-        this.setState({loadNews: true})
-    }
+    return (
+      <>
+        <SideNav />
 
-    render() {
+        <Container className="pt-lg-md main">
+          <Row className="justify-content-center">
+            <Col lg="10" className="category">
+              <h1>News</h1>
+              <div>
+                {!this.state.loadNews && <Loading />}
 
-        this.timeout = setTimeout(() => this.loadNews(), 1000);
+                {this.state.loadNews && (
+                  <>
+                    <Card className="shadow mb-4">
+                      <CardBody>
+                        <img
+                          alt="..."
+                          className="img-fluid lazyload"
+                          data-src={require(`assets/imgs/aliens-earth.svg`)}
+                        />
+                      </CardBody>
+                    </Card>
+                    <Card className="shadow mb-4">
+                      <CardBody>
+                        <img
+                          alt="..."
+                          className="img-fluid lazyload"
+                          data-src={require(`assets/imgs/deadlyplastic.png`)}
+                        />
+                      </CardBody>
+                    </Card>
+                  </>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 
-        return (
-            <>
-                <SideNav/>
-
-                <Container className="pt-lg-md main">
-                    <Row className="justify-content-center">
-                        <Col lg="10" className="category">
-                            <h1>News</h1>
-                            <div>
-
-                                {!this.state.loadNews && <Loading/>}
-
-
-                                {this.state.loadNews && (
-                                    <>
-                                        <Card className="shadow mb-4">
-                                            <CardBody>
-                                                <img
-                                                    alt="..."
-                                                    className="img-fluid"
-                                                    src={require(`assets/imgs/aliens-earth.svg`)}
-                                                />
-                                            </CardBody>
-                                        </Card>
-                                        <Card className="shadow mb-4">
-                                            <CardBody>
-                                                <img
-                                                    alt="..."
-                                                    className="img-fluid"
-                                                    src={require(`assets/imgs/deadlyplastic.png`)}
-                                                />
-                                            </CardBody>
-                                        </Card>
-                                    </>
-                                )}
-
-
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.timeout)
-    }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
 }
 
 export default News;
